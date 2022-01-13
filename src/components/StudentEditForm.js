1474b541cb3fdb3d403b147be887ebea53f6d0a2
Scott Love <scottlove@Claudias-MacBook-Pro.local>
2022-01-12 16:19:38 -0800

    updated studentedit component state

--- src/components/StudentEditForm.js
+++ src/components/StudentEditForm.js
@@ -1,10 +1,41 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import StudentContext from '../context/StudentContext';
 import { Form, Row, Col, Container, Button } from 'react-bootstrap';
 
 export default function StudentEditForm() {
-  const { name, phone, handleSubmit, handleNameChange, handlePhoneChange } =
-    useContext(StudentContext);
+  const { handleStudentAdd } = useContext(StudentContext);
+
+  const defaultImage =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgprFQGqxHFOm1UVGESj3v-16NVyvBiwiF-sHUGakrPBeDJ6Uh8uvJbDFMttF4dL1XGO8&usqp=CAU';
+
+  const [name, setName] = useState('');
+  const [image, setImage] = useState(defaultImage);
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [grade, setGrade] = useState('');
+  const [notes, setNotes] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newStudent = {
+      name,
+      image,
+      phone,
+      email,
+      grade,
+      notes,
+    };
+
+    handleStudentAdd(newStudent);
+
+    setName('');
+    setImage(defaultImage);
+    setPhone('');
+    setEmail('');
+    setGrade('');
+    setNotes('');
+  }
 
   return (
     <Container>
@@ -13,7 +44,7 @@ export default function StudentEditForm() {
           <Form.Group as={Col} htmlFor='name'>
             <Form.Label>Name</Form.Label>
             <Form.Control
-              onChange={handleNameChange}
+              onChange={(e) => setName(e.target.value)}
               type='text'
               value={name}
               placeholder='Full name'
@@ -30,7 +61,7 @@ export default function StudentEditForm() {
           <Form.Group as={Col} htmlFor='phone'>
             <Form.Label>Phone</Form.Label>
             <Form.Control
-              onChange={handlePhoneChange}
+              onChange={(e) => setPhone(e.target.value)}
               value={phone}
               type='text'
               name='phone'
@@ -40,12 +71,24 @@ export default function StudentEditForm() {
 
           <Form.Group as={Col} htmlFor='email'>
             <Form.Label>Email</Form.Label>
-            <Form.Control type='email' name='email' id='email' />
+            <Form.Control
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type='email'
+              name='email'
+              id='email'
+            />
           </Form.Group>
 
           <Form.Group as={Col} htmlFor='grade'>
             <Form.Label>Grade</Form.Label>
-            <Form.Control type='number' name='grade' id='grade' />
+            <Form.Control
+              onChange={(e) => setGrade(e.target.value)}
+              value={grade}
+              type='number'
+              name='grade'
+              id='grade'
+            />
           </Form.Group>
         </Row>
 
@@ -53,6 +96,8 @@ export default function StudentEditForm() {
           <Form.Label>Notes</Form.Label>
           <Form.Control
             style={{ resize: 'none' }}
+            onChange={(e) => setNotes(e.target.value)}
+            value={notes}
             name='notes'
             id='notes'
             as='textarea'
--- src/context/StudentContext.js
+++ src/context/StudentContext.js
@@ -5,69 +5,10 @@ const StudentContext = createContext();
 
 export const StudentProvider = ({ children }) => {
   const [students, setStudents] = useState(data);
-  const [name, setName] = useState('');
-  const [image, setImage] = useState(
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgprFQGqxHFOm1UVGESj3v-16NVyvBiwiF-sHUGakrPBeDJ6Uh8uvJbDFMttF4dL1XGO8&usqp=CAU'
-  );
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [grade, setGrade] = useState('');
-  const [notes, setNotes] = useState('');
-  // const [search, setSearch] = useState('');
-
-  // const filterStudents = students.filter((student) => {
-  //   return student.name.toLowerCase().includes(search.toLowerCase());
-  // });
-
-  // function handleSearchChange(e) {
-  //   setSearch(e.target.value);
-  // }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    const newStudent = {
-      name,
-      image,
-      phone,
-      email,
-      grade,
-      notes,
-    };
-
-    handleStudentAdd(newStudent);
-
-    setName('');
-    setPhone('');
-    setEmail('');
-    setGrade('');
-    setNotes('');
-  }
 
   function handleStudentAdd(newStudent) {
     newStudent.id = uuidv4();
     setStudents([newStudent, ...students]);
-  }
-
-  function handleNameChange(e) {
-    setName(e.target.value);
-  }
-
-  function handleImageChange(e) {
-    setImage(e.target.value);
-  }
-
-  function handlePhoneChange(e) {
-    setPhone(e.target.value);
-  }
-  function handleEmailChange(e) {
-    setEmail(e.target.value);
-  }
-  function handleGradeChange(e) {
-    setGrade(e.target.value);
-  }
-  function handleNotesChange(e) {
-    setNotes(e.target.value);
   }
 
   function handleStudentDelete(id) {
@@ -80,15 +21,8 @@ export const StudentProvider = ({ children }) => {
     <StudentContext.Provider
       value={{
         students,
-        name,
-        phone,
-        // filterStudents,
-        // handleSearchChange,
-        handleNameChange,
-        handlePhoneChange,
         handleStudentAdd,
         handleStudentDelete,
-        handleSubmit,
       }}
     >
       {children}
